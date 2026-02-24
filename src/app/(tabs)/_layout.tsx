42f85a7981edcca80770b600214531551162e09d
Andres <argomez.restrepo@gmail.com>
2026-02-24 18:58:13 -0500

    [Navigation]: Add Expo router and tabs navigation

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,45 @@
 import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
 
 export default function TabsLayout() {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="workshop"
+        options={{
+          title: "Workshop",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="pencil" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="user" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="cog" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
