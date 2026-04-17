30af00d6109046f9326eaf61fe105fd70ba8cd8b
Andres Gomez <79469076+ricgomez99@users.noreply.github.com>
2026-04-17 17:50:50 -0500

    Styled SearchBar and added to the workshop screens (#4)

--- src/components/shared/SearchBar/index.tsx
+++ src/components/shared/SearchBar/index.tsx
@@ -1,5 +1,4 @@
-import { View, TextInput } from "react-native";
-
+import { View, TextInput, StyleSheet } from "react-native";
 interface SearchBarProps {
   value: string;
   handleChange: (text: string) => void;
@@ -7,8 +6,9 @@ interface SearchBarProps {
 
 export default function SearchBar({ value, handleChange }: SearchBarProps) {
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
+        style={styles.searchBar}
         defaultValue={value}
         onChangeText={handleChange}
         clearButtonMode="always"
@@ -17,3 +17,16 @@ export default function SearchBar({ value, handleChange }: SearchBarProps) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 5,
+  },
+
+  searchBar: {
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 10,
+  },
+});
