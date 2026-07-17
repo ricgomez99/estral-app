d29e3cbfcd2288a5be4379ea957d8d438e703c24
Andres <argomez.restrepo@gmail.com>
2026-07-17 16:54:22 -0500

    Added native DatePicker components

---
+++ src/components/shared/DatePickerCustom/DatePickerCustom.d.ts
@@ -0,0 +1,5 @@
+import { ComponentType } from "react";
+import { IDatePickerProps } from "@/types/shared-types/date-picker-types";
+
+declare const DatePickerCustom: ComponentType<IDatePickerProps>;
+export default DatePickerCustom;
